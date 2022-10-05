a0eb0c7e3e4237cb497d6f4663cc0e6db38a2bf7
Youssef Auassar <youssef.auassar@student.ehb.be>
2022-10-05 12:28:40 +0200

    Exercise 1 update

--- exercises/1-functions/1-1-name.js
+++ exercises/1-functions/1-1-name.js
@@ -1,1 +1,36 @@
+
 "use strict";
+
+alert('Oefening 1');
+
+let canvas = document.querySelector ("canvas");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+let context = canvas.getContext("2d");
+
+drawName();
+
+function drawName () {
+ 
+    context.lineWidth = 7;
+    context.strokeStyle = 'red';
+    context.beginPath();
+    context.moveTo(50, 100);
+    context.lineTo(200, 300);
+    
+    context.lineTo(400, 100);
+
+    context.lineTo(100, 400);
+
+    context.stroke();
+
+    
+
+
+
+
+
+ 
+     
+}
+
